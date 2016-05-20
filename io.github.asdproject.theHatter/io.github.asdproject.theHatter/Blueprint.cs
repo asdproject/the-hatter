e3b7579a3fd642defda5625afb7a6a6fc5fc0031
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-05-20 09:53:23 +0200

    update

--- io.github.asdproject.theHatter/io.github.asdproject.theHatter/Blueprint.cs
+++ io.github.asdproject.theHatter/io.github.asdproject.theHatter/Blueprint.cs
@@ -4,6 +4,13 @@
 
     public class Blueprint<T> : IBlueprint<T>
     {
+        private string _sourceCode;
+        
         public T Object { get; private set; }
+        
+        public Blueprint()
+        {
+            
+        }
     }
 }
--- io.github.asdproject.theHatter/io.github.asdproject.theHatter/Program.cs
+++ io.github.asdproject.theHatter/io.github.asdproject.theHatter/Program.cs
@@ -1,12 +1,48 @@
-﻿//namespace io.github.asdproject.theHatter
-//{
-//    using System;
+﻿namespace io.github.asdproject.theHatter
+{
+    using System;
+    using interfaces;
 
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Console.WriteLine("asd");
-//        }
-//    }
-//}
+    public class Test
+    {
+        private readonly IBlueprint<IInterface> _blueprint;
+        
+        public Test()
+        {
+            // Arrange
+            _blueprint = new Blueprint<IInterface>();
+            
+            NotNull();
+            ObjectNotNull();
+        }
+        
+        public void NotNull()
+        {
+            // Assert
+            if(_blueprint == null)
+                throw new Exception();
+        }
+        
+        public void ObjectNotNull()
+        {
+            // Assert
+            if(_blueprint.Object == null)
+                throw new NullReferenceException(nameof(_blueprint.Object));
+        }
+    }
+    
+    public interface IInterface
+    {
+        
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test = new Test();
+                        
+            Console.WriteLine("SUCESS");
+        }
+    }
+}
